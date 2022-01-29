d087dc3101eb69b83ce077ff32b7ae8ba9c45f27
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2022-01-28 21:10:17 -0500

    Initial push
    Push up in-progress code

--- Assignment1/src/Driver.java
+++ Assignment1/src/Driver.java
@@ -1,4 +1,71 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.BitSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * @author Tanya
+ */
 public class Driver {
+	private static final int MAX_PRIME_SIZE = 100_000_000;
+	private static final int THREAD_COUNT = 2;
+
+	private static void findMultiples(BitSet arr, AtomicInteger sharedCounter) {
+		int p;
+		while ((p = sharedCounter.getAndIncrement()) < MAX_PRIME_SIZE)
+			for (int i = 2 * p; i < MAX_PRIME_SIZE; i += p)
+				arr.set(i);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		AtomicInteger sharedCounter = new AtomicInteger(2);
+
+		Thread[] threads = new Thread[THREAD_COUNT];
+		BitSet[] arr = new BitSet[threads.length];
+
+		for (int i = 0; i < threads.length; i++) {
+			BitSet b = new BitSet(MAX_PRIME_SIZE);
+			arr[i] = b;
+			threads[i] = new Thread(() -> findMultiples(b, sharedCounter));
+		}
+
+		Instant start = Instant.now();
+
+		for (Thread t : threads)
+			t.start();
+		for (Thread t : threads)
+			t.join();
+
+		BitSet res = new BitSet(MAX_PRIME_SIZE);
+		res.set(0, MAX_PRIME_SIZE);
+		for (int i = 0; i < arr.length; i++)
+			res.andNot(arr[i]);
+
+		Duration exectime = Duration.between(start, Instant.now());
+
+		reportFoundPrimes(MAX_PRIME_SIZE, res, exectime);
+	}
+
+	private static void reportFoundPrimes(int n, BitSet arr, Duration exectime) {
+		int count = 0;
+		long sum = 0;
+
+		for (int i = 2; i < n && i > 0; i = arr.nextSetBit(i + 1)) {
+			count++;
+			sum += i;
+		}
+
+		int[] top = new int[10];
+
+		for (int i = top.length - 1, prime = arr.previousSetBit(arr.length()); i >= 0; i--, prime = arr
+				.previousSetBit(prime - 1))
+			top[i] = prime;
+
+		System.out.println(exectime.getSeconds() + "." + String.format("%03d", exectime.getNano() / 1_000_000) + " "
+				+ count + " " + sum + "\n");
+
+		for (int i : top)
+			System.out.println(i);
+	}
 
 }
